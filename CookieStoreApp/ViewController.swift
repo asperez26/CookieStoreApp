6d73fbaf6a32ed0b0b621ddbb2e68f72dfdb6fac
asaperez26 <asperez@myseneca.ca>
2025-02-18 12:57:32 -0500

    Initial Commit

---
+++ CookieStoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookieStoreApp
+//
+//  Created by Andrea Selina Perez on 2025-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
